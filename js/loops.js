04954e3bdce560a9f9df500a1e1ed22e757a524b
epait <ericpait@gmail.com>
2013-06-18 16:13:20 -0400

    Adding buttons to advance transition text

--- js/loops.js
+++ js/loops.js
@@ -7,7 +7,7 @@ function createPausePoint(selection) {
     .append('div')
     .attr('class', 'videoLoopContainer')
     .attr('id', function (d) {
-                    return d.transitionLoop;
+        return d.transitionLoop;
     })
    .style('display', 'none');
     
@@ -28,21 +28,36 @@ function createPausePoint(selection) {
                     .append('source')
                     .attr('src', function (t) { return './vid/' + d.transitionLoop + '.' + t.s; })
                     .attr('type', function (t) { return 'video/' + t.t; });
-
-            videoLoopContainer.selectAll('.textOverlay').data(function(d) { return d.transitionText; }).enter()
+            videoLoopContainer.selectAll('.textOverlay').data(function(d) { return  d.transitionText; }).enter()
                 .append('div')
                 .attr('class', 'textOverlay')
                 .attr('id', function(d, i) { return "sentence"+(i+1);})
                 .style('display', function(d, i) { return i == 0 ? 'block' : 'none'})
-                .on('click', function(td, ti) { videoLoopContainer.selectAll('.textOverlay').data(function(d) { return d.transitionText; }).transition().style('display', function(d, i) { return i = ti+1 ? 'block': 'none'; })})
-
-                .text(String);
-
+                .text(String)
+                .append('button')
+                    .attr('class','advanceSlide')
+                    .attr('id',function(d, i) { return "button"+(i+1);})
+                    .text('Next!');
         });
 
 }
 
 var selection = d3.select('.overlayWrapper').selectAll('.videoLoopContainer').data(sequence).enter();
 createPausePoint(selection);
+
+        $('#button1').on('click',function(){
+            console.log('Click!');
+            $('#button1').fadeOut(500);
+            $('#sentence1').fadeOut(500);
+            $('#sentence2').fadeIn(1000);
+            $('#button2').fadeIn(1000);
+        });
+        $('#button2').on('click',function(d){
+            $('#button2').fadeOut(500);
+            $('#sentence2').fadeOut(500);
+            $('.videoLoopContainer').fadeOut(1000);
+            $('#button1').show();
+            console.log('Click2!');
+        });
 
 
